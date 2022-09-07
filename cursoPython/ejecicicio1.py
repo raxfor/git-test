9161713dd13a65f9ca8edd64c08d742008550cb3
raxfor <jhonalexslb@gmail.com>
2022-09-06 21:11:51 -0500

    feat: ejercicio1 curso python

---
+++ cursoPython/ejecicicio1.py
@@ -0,0 +1,24 @@
+nombre = "Alexander"
+apellidos = "Sanchez Rodriguez"
+edad = 24
+email = "prueba@gmail.com"
+telefono = 3231232
+casado = True
+conHijos = False
+listaAmigos = ["carlos", "jose", "andres", "eduardo", "alfonso"]
+peliculasVistas = {
+    "Aventura" : "los increibles",
+    "Infantil" : "kunfu panda" ,
+    "Comedia" : "niñera a prueba de balas",
+    "Accion": "los indestructibles",
+}
+
+print("Nombre: ", nombre)
+print("Apellidos: ", apellidos)
+print("Edad : ",  edad)
+print("Email : ", email)
+print("Telefono : ", telefono)
+print("Casado: ", casado)
+print("Con Hijos : ", conHijos)
+print("Lista Amigos : ", listaAmigos)
+print("Peliculas Vistas : ", peliculasVistas)
