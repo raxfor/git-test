d67b33ce3d440350475957c2e242949275a72acf
raxfor <jhonalexslb@gmail.com>
2022-09-08 15:41:41 -0500

    feat: primer ejercicio de modulos curso python

---
+++ cursoPython/ejecicioModulos.py
@@ -0,0 +1,11 @@
+from operations.operaciones import *
+
+
+suma = sumar(4,5)
+print("El resultado de la suma es : " ,suma)
+resta = restar(5,2)
+print("El resultado de la resta es : ", resta)
+multiplica = multiplicar(5,2)
+print("El resultado de la multiplicacion es : ", multiplica)
+divide = dividir(5,2)
+print("El resultado de la division es : ", divide)
